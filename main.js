7341ff692beb2b5a03dbf5c0228f314088b816e6
ubai-d <ubaidurrehman14827@gmail.com>
2024-04-04 00:50:50 +0500

    data types coverd

---
+++ main.js
@@ -0,0 +1,22 @@
+// let name1 = "variable by let";
+// console.log(name1)
+// let name1 = "Ubaid"
+// console.log(name1)
+// var name1 = "Variable by var"
+// var name1 = "Ubaid"
+// console.log(name1)
+// const name1 = "Variable by const"
+// name1 = "Ubaid"
+// console.log(name1)
+var name1 = "Ubaid"; // infering type
+var name2 = "Ubaid"; // strongly typed
+// name2 = 1000    errors 
+var num1 = 1000; // infering type
+var num2 = 1000; // strongly typed 
+var news = false; // infering type
+var news1 = true; // strongly typed
+var anyData = 1000; // not recommended
+console.log(anyData);
+anyData = "Hello";
+anyData = true;
+console.log(anyData);
---
+++ main.ts
@@ -0,0 +1,30 @@
+// let name1 = "variable by let";
+// console.log(name1)
+// let name1 = "Ubaid"
+// console.log(name1)
+
+// var name1 = "Variable by var"
+// var name1 = "Ubaid"
+// console.log(name1)
+
+
+// const name1 = "Variable by const"
+// name1 = "Ubaid"
+// console.log(name1)
+
+
+let name1 = "Ubaid"    // infering type
+let name2:string = "Ubaid"  // strongly typed
+// name2 = 1000    errors 
+let num1 = 1000  // infering type
+let num2:number = 1000  // strongly typed 
+let news = false  // infering type
+let news1:boolean = true // strongly typed
+let anyData:any = 1000  // not recommended
+console.log(anyData);
+
+anyData = "Hello"
+anyData = true
+
+console.log(anyData);
+
